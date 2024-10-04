f15f95ad5bc30a5625f20aee3003fb161780bda9
automatickk <automatick07@gmail.com>
2024-10-04 08:34:49 +0300

    Zoo 3 stage.

--- Zoo/zoo.py
+++ Zoo/zoo.py
@@ -1,11 +1,16 @@
+import re
+
+#stage 1
 print("""
 I love animals!
 Let's check out the animals...
 The deer looks fine.
 The lion looks healthy.
 """)
+
 # stage 2
 print(r"""
+The camel habitat...
     _______\\__
    (_. _ ._  _/
     '-' \__. /
@@ -19,8 +24,23 @@ print(r"""
               \| /       )|\
                |/       // \\
                |\    __//   \\__
-              //\\  /__/  mrf\__|
+              //\\  /__/     \__|
           .--_/  \_--.
          /__/      \__\
 Look at that!
 """)
+#3 stage
+with open("arts.txt", "r") as f:
+    fstr = f.read()
+    pattern = r'index:(\d+)/name:([a-zA-Z]+)\n((?:.|\n)+?)(?=index|$)'
+    matches = re.findall(pattern, fstr)
+
+indexes = dict()
+for match in matches:
+    indexes[match[0]] = [match[1], match[2]]
+
+print("Please enter the number of the habitat you would like to view.")
+index = input("-> ")
+name = indexes[index][0]
+art = indexes[index][1]
+print(f"Showing the {name} habitat...\n{art}")
