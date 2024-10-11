f0a9873468a9b9a4a9b62590953a4af7e5bba8ae
automatickk <automatick07@gmail.com>
2024-10-11 09:57:32 +0300

    First commit

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,1 +1,8 @@
-print("HANGMAN\nThe game will be available soon.")
+import random
+
+print("HANGMAN")
+words: list = ["python", "java", "c"]
+if input("Guess the word -> ") == random.choice(words):
+    print("you survived")
+    exit(0)
+print("you dead")
