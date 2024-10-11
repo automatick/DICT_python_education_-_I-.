c3f88a288a2b4aa2f23c1dcbce0bb7ac09909051
automatickk <automatick07@gmail.com>
2024-10-11 10:13:11 +0300

    3 stage

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,8 +1,12 @@
 import random
 
 print("HANGMAN")
-words: list = ["python", "java", "c"]
-if input("Guess the word -> ") == random.choice(words):
+words: list = ["python", "java", "php", "javascript"]
+
+random_word: list = random.choice(words)
+
+if input(f"Guess the word {random_word[:3] + (len(random_word) - 3) * '-'} -> ") == random_word:
     print("you survived")
-    exit(0)
-print("you dead")
+
+else:
+    print("you dead")
