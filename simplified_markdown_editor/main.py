0beae1c5dda185f2fe73c0dc2d872b4681b72a5c
automatickk <automatick07@gmail.com>
2025-02-05 08:10:30 +0200

    fix2

--- simplified_markdown_editor/main.py
+++ simplified_markdown_editor/main.py
@@ -1,56 +1,4 @@
-import sys
-
-class MarkdownFormatter:
-    @staticmethod
-    def plain(text):
-        return text
-
-    @staticmethod
-    def bold(text):
-        return f"**{text}**"
-
-    @staticmethod
-    def italic(text):
-        return f"*{text}*"
-
-    @staticmethod
-    def inline_code(text):
-        return f"`{text}`"
-
-    @staticmethod
-    def header(text):
-        while True:
-            try:
-                level = int(input("Level: > "))
-                if 1 <= level <= 6:
-                    return f"{'#' * level} {text}"
-                print("The level should be within the range of 1 to 6.")
-            except ValueError:
-                print("Invalid input. Please enter a number.")
-
-    @staticmethod
-    def link():
-        label = input("Label: > ")
-        url = input("URL: > ")
-        return f"[{label}]({url})"
-
-    @staticmethod
-    def new_line():
-        return "\n"
-
-    @staticmethod
-    def list_formatter(formatter):
-        try:
-            rows = int(input("Number of rows: > "))
-            if rows <= 0:
-                print("The number of rows should be greater than zero.")
-                return ""
-            items = [input(f"Row #{i + 1}: > ") for i in range(rows)]
-            prefix = "- " if formatter == "unordered-list" else ""
-            return "\n".join([f"{prefix}{i + 1}. {item}" if formatter == "ordered-list" else f"* {item}" for i, item in enumerate(items)]) + "\n"
-        except ValueError:
-            print("Invalid input. Please enter a number.")
-            return ""
+from markdown_formatter import *
 
 # Функція для виводу довідки
 
@@ -63,15 +11,15 @@ def print_help():
 def main():
     markdown = ""
     formatter_map = {
-        "plain": MarkdownFormatter.plain,
-        "bold": MarkdownFormatter.bold,
-        "italic": MarkdownFormatter.italic,
-        "inline-code": MarkdownFormatter.inline_code,
-        "header": MarkdownFormatter.header,
-        "link": MarkdownFormatter.link,
-        "new-line": MarkdownFormatter.new_line,
-        "ordered-list": MarkdownFormatter.list_formatter,
-        "unordered-list": MarkdownFormatter.list_formatter
+        "plain": plain,
+        "bold": bold,
+        "italic": italic,
+        "inline-code": inline_code,
+        "header": header,
+        "link": link,
+        "new-line": new_line,
+        "ordered-list": list_formatter,
+        "unordered-list": list_formatter
     }
     
     while True:
